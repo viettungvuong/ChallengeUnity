b066fcc516cff65728ee8d0d6d77920a3cee9ec6
viettung <viettungvuong@gmail.com>
2022-07-13 22:43:02 +0700

    Thêm tường vô hình xung quanh khu vực chơi

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -4,9 +4,25 @@ using UnityEngine;
 
 public class Game : MonoBehaviour
 {
+    const int spawn = 10;
     public static int score = 0;
+    public TMPro.TextMeshProUGUI scoreText;
+    public List<GameObject> enemies;
+    private void Update()
+    {
+        scoreText.text = Game.score.ToString();
+    }
     public static void incrementScore()
     {
         Game.score++;
+    }
+    void spawnEnemies()
+    {
+        GameObject[] temp = GameObject.FindGameObjectsWithTag("enemy");
+        enemies = new List<GameObject>(temp);
+        for (int i = 0; i < spawn; i++)
+        {
+
+        }
     }
 }
