7bb5aa3d7ac496a6ef0ffce6bda40b34f923935f
viettung <viettungvuong@gmail.com>
2022-07-13 22:50:09 +0700

    Thêm cái để spawn enemy

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Game : MonoBehaviour
 {
     const int spawn = 10;
+    const float ySpawn = -4.768372e-07f;
+    const float xLow = 3.6f, xHigh = 96.8f;
+    const float zLow = 8.3f, zHigh = 97.16f;
     public static int score = 0;
     public TMPro.TextMeshProUGUI scoreText;
     public List<GameObject> enemies;
